c1cc93f58e27823d161b625e67b1e7a3171778af
Unknown <omarko.kamel@gmail.com>
2018-06-27 23:17:06 +0200

    initialization and Interrupt
    the interrupt is working but there is no sensor yet

---
+++ sketch_jun24a.ino
@@ -0,0 +1,24 @@
+
+const byte ledPin = 13;
+const byte interruptPin = 2;
+volatile byte state = LOW;
+long lastDebounceTime=0;
+long debounceDelay=50;
+
+void blink() {
+  if ((millis()-lastDebounceTime)>debounceDelay)
+  {
+  state = !state;
+  lastDebounceTime=millis();
+  }
+}
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), blink, RISING);
+
+}
+
+void loop() {
+  digitalWrite(ledPin, state);
+}
