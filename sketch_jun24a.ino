60d1cabba7b9d68afae59e488a2b14245f93e062
Unknown <omarko.kamel@gmail.com>
2018-06-29 12:06:33 +0200

    Added sensor code

--- sketch_jun24a.ino
+++ sketch_jun24a.ino
@@ -1,10 +1,12 @@
 
 const byte ledPin = 13; 
+const byte sensorpin= A1;
 const byte interruptPin = 2; //the interrupt pin is 2
 volatile byte state = LOW;  //determining the led state
 long lastDebounceTime=0;   //last debounce time to overcome the bouncing effect
-long debounceDelay=50;   //needed delay to overcome bouncing effect
-
+long debounceDelay=100;   //needed delay to overcome bouncing effect
+long reading_rate=3000; // 3 secns rate
+long last_read=0;
 
 void setup() {
   pinMode(ledPin, OUTPUT);  // the led pin is output
@@ -16,8 +18,17 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(ledPin, state);
-  //sensor code
+  digitalWrite(ledPin, state); // showing the led status form the last interrupt
+  //sensor code , rate= 3sec
+  if((millis()-last_read)>=reading_rate)//
+  {
+    last_read=millis();
+    Serial.print(temp_read());
+    Serial.print('\n');
+    
+  }
+  
+  
 }
 
 void blink() {
@@ -31,4 +42,12 @@ void blink() {
   if(state==LOW) Serial.print("LED IS OFF\n");
   else Serial.print("LED IS ON\n");
   }
+
+  
 }
+float temp_read(){
+int  val=analogRead(sensorpin); //reading the register value from the ADC
+  float mv = ( val/1024.0)*5000; //knwoing the corresponding mV to this value, max is 5000mv that corresponds to 1024
+  float cel = (mv/10); //converting the mv to temprature,,, 1c=10mv
+  return cel;
+  }
